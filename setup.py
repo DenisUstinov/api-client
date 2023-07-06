bba9881d755ab0acd535f8596bf09c5799b43450
Denis Ustinow <ml_engineer@icloud.com>
2023-07-06 17:06:57 +0700

    Fix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='api-client',
+    name='api-clients',
     version='0.1.1',
     author='ChatGPT and Denis Ustinov',
     author_email='revers-06-checkup@icloud.com',
-    description='A Python package for working with API',
+    description='A Python package for working with APIs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/DenisUstinov/exmo',
+    url='https://github.com/DenisUstinov/api-clients',
     license='MIT',
     packages=find_packages(),
     install_requires=['websockets', 'backoff', 'aiohttp'],

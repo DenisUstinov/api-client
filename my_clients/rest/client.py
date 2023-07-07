63861cee59774cccd1aade57d2ad73186425b3cf
Denis Ustinow <ml_engineer@icloud.com>
2023-07-07 09:37:30 +0700

    Reformat project

--- my_clients/rest/client.py
+++ my_clients/rest/client.py
@@ -1,24 +1,15 @@
-import logging
 import backoff
 import aiohttp
-
-from my_clients import RequestError
-from my_clients import setup_logger
 
 
 class Client:
     def __init__(self) -> None:
         self.session = aiohttp.ClientSession()
-        self.logger = setup_logger(__name__, 'errors.log')
 
     @backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=5)
     async def request(self, method: str, url: str, headers: dict = None, data: dict = None):
-        try:
-            async with self.session.request(method, url, headers=headers, data=data) as response:
-                return await response.text()
-        except aiohttp.ClientError as e:
-            self.logger.exception('Error fetching URL')
-            raise RequestError('Error fetching URL', url=url, method=method, request_body=data) from e
+        async with self.session.request(method, url, headers=headers, data=data) as response:
+            return await response.text()
 
     async def close(self):
         await self.session.close()
--- my_clients/websocket/client.py
+++ my_clients/websocket/client.py
@@ -1,53 +1,29 @@
-import logging
-
 import backoff
 import websockets
 import websockets.exceptions
 
 from typing import Dict, Any, Callable, Coroutine
 
-from my_clients import RequestError
-from my_clients import setup_logger
-
 
 class Client:
-    """WebSocket client class.
-
-    Attributes:
-        response_handler (Callable[[dict], Coroutine]): The coroutine function to handle responses.
-        logger (logging.Logger): The logger instance for error logging.
-        file_handler (logging.FileHandler): The file handler for error log file.
-    """
-
     def __init__(self, response_handler: Callable[[dict], Coroutine]) -> None:
-        """Initialize the WebSocket client.
-
-        Args:
-            response_handler (Callable[[dict], Coroutine]): The coroutine function to handle responses.
-        """
         self.response_handler = response_handler
-        self.logger = setup_logger(__name__, 'errors.log')
 
     @backoff.on_exception(backoff.expo, websockets.exceptions.WebSocketException, max_tries=5)
     async def subscribe_and_listen(self, data: Dict[str, Any]) -> None:
-        """Subscribe to a WebSocket and listen for messages.
+        url = data.get('url')
+        if url is None:
+            raise ValueError('url is not provided')
 
-        Args:
-            data (Dict[str, Any]): The data containing the URL and optional initial messages.
-        """
-        try:
-            url = data.get('url')
-            if url is None:
-                raise RequestError('URL is missing')
+        async with websockets.connect(url) as websocket:
+            init_messages = data.get('init_messages')
+            if init_messages is None:
+                raise ValueError('init_messages is not provided')
 
-            async with websockets.connect(url) as websocket:
-                init_messages = data.get('init_messages', [])
+            if init_messages:
                 for message in init_messages:
                     await websocket.send(message)
 
                 while True:
                     message = await websocket.recv()
                     await self.response_handler(message)
-        except websockets.WebSocketException as e:
-            self.logger.exception('Error fetching URL')
-            raise RequestError('Error fetching URL', url=url, method=None, request_body=data) from e

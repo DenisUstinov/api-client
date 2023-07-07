0f8adeac69e285754d5627f0d3e5d8fca493857c
Denis Ustinow <ml_engineer@icloud.com>
2023-07-07 07:37:41 +0700

    Reformat project

--- my_clients/rest/client.py
+++ my_clients/rest/client.py
@@ -1,23 +1,33 @@
-import json
+import logging
 import backoff
 import aiohttp
+
+
+class RequestError(Exception):
+    def __init__(self, message, url=None, method=None, request_body=None):
+        super().__init__(message)
+        self.url = url
+        self.method = method
+        self.request_body = request_body
 
 
 class Client:
     def __init__(self) -> None:
         self.session = aiohttp.ClientSession()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.ERROR)
+        self.file_handler = logging.FileHandler('errors.log')
+        self.file_handler.setLevel(logging.ERROR)
+        self.logger.addHandler(self.file_handler)
 
     @backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=5)
-    async def request(self, method: str, url: str, headers: dict = None, data: dict = None) -> dict:
+    async def request(self, method: str, url: str, headers: dict = None, data: dict = None):
         try:
             async with self.session.request(method, url, headers=headers, data=data) as response:
-                try:
-                    response_obj = await response.json()
-                    return response_obj
-                except json.JSONDecodeError:
-                    raise ValueError("Invalid JSON object received")
+                return await response.text()
         except aiohttp.ClientError as e:
-            raise ValueError(f'Error fetching {url}: {str(e)}')
+            self.logger.exception('Error fetching URL')
+            raise RequestError('Error fetching URL', url=url, method=method, request_body=data) from e
 
     async def close(self):
         await self.session.close()
--- my_clients/websocket/client.py
+++ my_clients/websocket/client.py
@@ -1,4 +1,4 @@
-import json
+import logging
 
 import backoff
 import websockets
@@ -7,20 +7,65 @@ import websockets.exceptions
 from typing import Dict, Any, Callable, Coroutine
 
 
+class RequestError(Exception):
+    """Exception raised for request errors.
+
+    Attributes:
+        message (str): The error message.
+        url (str): The URL that caused the error.
+        method (str): The request method.
+        request_body (Any): The request body.
+    """
+
+    def __init__(self, message: str, url: str = None, method: str = None, request_body: Any = None):
+        super().__init__(message)
+        self.url = url
+        self.method = method
+        self.request_body = request_body
+
+
 class Client:
+    """WebSocket client class.
+
+    Attributes:
+        response_handler (Callable[[dict], Coroutine]): The coroutine function to handle responses.
+        logger (logging.Logger): The logger instance for error logging.
+        file_handler (logging.FileHandler): The file handler for error log file.
+    """
+
     def __init__(self, response_handler: Callable[[dict], Coroutine]) -> None:
+        """Initialize the WebSocket client.
+
+        Args:
+            response_handler (Callable[[dict], Coroutine]): The coroutine function to handle responses.
+        """
         self.response_handler = response_handler
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.ERROR)
+        self.file_handler = logging.FileHandler('errors.log')
+        self.file_handler.setLevel(logging.ERROR)
+        self.logger.addHandler(self.file_handler)
 
     @backoff.on_exception(backoff.expo, websockets.exceptions.WebSocketException, max_tries=5)
     async def subscribe_and_listen(self, data: Dict[str, Any]) -> None:
-        async with websockets.connect(data['url']) as websocket:
-            for message in data.get('init_messages', []):
-                await websocket.send(message)
+        """Subscribe to a WebSocket and listen for messages.
 
-            while True:
-                response = await websocket.recv()
-                try:
-                    response_obj = json.loads(response)
-                except json.JSONDecodeError:
-                    raise ValueError("Invalid JSON object received")
-                await self.response_handler(response_obj)
+        Args:
+            data (Dict[str, Any]): The data containing the URL and optional initial messages.
+        """
+        try:
+            url = data.get('url')
+            if url is None:
+                raise RequestError('URL is missing')
+
+            async with websockets.connect(url) as websocket:
+                init_messages = data.get('init_messages', [])
+                for message in init_messages:
+                    await websocket.send(message)
+
+                while True:
+                    message = await websocket.recv()
+                    await self.response_handler(message)
+        except websockets.WebSocketException as e:
+            self.logger.exception('Error fetching URL')
+            raise RequestError('Error fetching URL', url=url, method=None, request_body=data) from e
